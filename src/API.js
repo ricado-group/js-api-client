2b44520c7dbed72cc6e8899041469d8e4ec1c77c
Ash Neilson <ash@ricado.co.nz>
2018-02-11 14:23:42 +1300

    Working on getting the environment setup so we can transpile down to ES5 and complete unit tests with coverage.

---
+++ src/API.js
@@ -0,0 +1,12 @@
+import blah from './toplevel';
+import blah2 from './down/a/few/levels';
+
+//export class API
+class API
+{
+
+}
+
+export default API
+//export { API }
+//module.exports = exports = API;
