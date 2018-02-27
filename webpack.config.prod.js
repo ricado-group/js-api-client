d899975f7a1398a95cd1193674380ed073bf6739
Mike Lee <mike@ricado.co.nz>
2018-02-27 14:15:39 +1300

    Author library by exposing RicadoAPI, as per Webpack docs: https://webpack.js.org/guides/author-libraries/

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -4,28 +4,11 @@ module.exports = {
    entry: './dist/es5/index.js',
    output: {
       path: __dirname + '/dist/browser/',
-      filename: 'gen4.ricado.api.client.js'
+      filename: 'gen4.ricado.api.client.js',
+      library: 'RicadoAPI',
+      libraryTarget: 'var'
    },
-   module: {
-      /*
-         // Uncomment this to have the JS linter run
-         preLoaders: [
-         {
-         test: /\.jsx$|\.js$/,
-         loader: 'eslint-loader',
-         include: __dirname,
-         exclude: /bundle\.js$/
-         }
-         ],
-         */
-         /*
-         loaders: [
-         { test: /\.css$/, loader: 'style-loader!css-loader' },
-         { test: /\.(js|jsx)$/, exclude: /node_modules/, loader: "babel-loader" },
-         { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000'}
-         ]
-      */
-   },
+   module: {},
    plugins: [
       /* new webpack.optimize.UglifyJsPlugin({ mangle: true, sourcemap: false }), // This is now on by default in production mode */
       new webpack.DefinePlugin({
