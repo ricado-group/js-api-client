f45ec2ae31cf999053f9438c37a97c2b52c5b325
Ash Neilson <ash@ricado.co.nz>
2018-02-27 02:25:45 +0000

    webpack.config.prod.js edited online with Bitbucket

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -3,9 +3,9 @@ const webpack = require('webpack'); // Access built-in plugins
 module.exports = {
    entry: './dist/es5/index.js',
    output: {
-      path: __dirname + '/dist/browser/',
-      filename: 'gen4.ricado.api.client.js',
-      library: 'RicadoAPI',
+      path: path.resolve(__dirname, 'dist/browser'),
+      filename: 'ricado.api.client.js',
+      library: 'RICADO Gen 4 API',
       libraryTarget: 'var'
    },
    module: {},
