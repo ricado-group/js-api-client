34225ae38439ad330388dc6db58d48d63a4d9a76
Ash Neilson <ash@ricado.co.nz>
2018-03-02 12:57:22 +1300

    Added support for WebSockets! Handled the Connection and Re-connecting of the WebSockets along with subscribing to sites.
    Created a Points class that provides methods for Reading and Writing point values.

---
+++ src/Points.js
@@ -0,0 +1,364 @@
+import WebSocketHelper from './WebSocketHelper';
+import RequestHelper from './RequestHelper';
+import { isDefined, hasToken } from './index';
+import { EventEmitter } from 'events';
+import PointController from './Controllers/Site/PointController';
+
+/**
+ * This Class provides Methods to interact with Points on RICADO Gen 4
+ *
+ * @public
+ */
+class Points {
+
+    /**
+     * Initialize
+     * 
+     * @package
+     */
+    static initialize()
+    {
+        if(isDefined(Points._emitter) != true)
+        {
+            Points._emitter = new EventEmitter();
+        }
+
+        if(isDefined(Points._definitions) != true)
+        {
+            Points._definitions = {};
+        }
+
+        if(isDefined(Points._values) != true)
+        {
+            Points._values = {};
+        }
+
+        WebSocketHelper.on('readpoints', (key, readPoints) => {
+            if(isDefined(key) && key.includes('.') && readPoints.length > 0)
+            {
+                let keyId = key.split('.')[1];
+                let siteId = undefined;
+                
+                if(key.startsWith("site."))
+                {
+                    Points.log("Received `" + readPoints.length + "` Read Points for Site ID: " + keyId);
+
+                    siteId = keyId;
+                }
+                else if(key.startsWith("rtu."))
+                {
+                    Points.log("Received `" + readPoints.length + "` Read Points for RTU ID: " + keyId);
+
+                    siteId = this.getDefaultSiteId();
+                }
+
+                if(isDefined(siteId))
+                {
+                    if((siteId in Points._values) != true)
+                    {
+                        Points._values[siteId] = {};
+                    }
+
+                    readPoints.forEach((readPoint) => {
+                        Points._values[siteId][readPoint.id] = readPoint;
+                    });
+
+                    // TODO: We maybe should create our own array of changed points with additional data from this Class?
+                    //       e.g. Value Type and Name could be added?
+                    Points._emitter.emit('readpoints', siteId, readPoints);
+                }
+            }
+        });
+    }
+
+    /**
+     * Loggging
+     * 
+     * @private
+     * @param {string} message - The Message to Log
+     * @param {string} type - The Log Type (defaults to log)
+     */
+    static log(message, type = 'log')
+    {
+        switch(type)
+        {
+            case 'error':
+                console.error('Points :: ' + message);
+                break;
+            
+            case 'warn':
+            case 'warning':
+                console.warn('Points :: ' + message);
+                break;
+            
+            case 'log':
+            default:
+                console.log('Points :: ' + message);
+                break;
+        }
+    }
+
+    /**
+     * Subscribe to a Site for Points
+     * 
+     * @public
+     * @param {number} siteId - The Site ID
+     * @return {Promise<Boolean>}
+     */
+    static subscribe(siteId)
+    {
+        WebSocketHelper.subscribe('site.' + siteId);
+
+        if(hasToken() != true)
+        {
+            throw new Error("Points.subscribe cannot be called before Authentication has been successful");
+        }
+
+        return new Promise((resolve, reject) => {
+            Points.loadPointDefinitions(siteId)
+            .then(() => {
+                Points.loadPointValues(siteId)
+                .then(() => {
+                    resolve(true);
+                })
+                .catch(() => {
+                    reject(new Error("Failed to Subscribe to Site ID " + siteId + ". Unable to Fetch the Point Values"));
+                });
+            })
+            .catch(() => {
+                reject(new Error("Failed to Subscribe to Site ID " + siteId + ". Unable to Fetch the Point Definitions"));
+            });
+        });
+    }
+
+    /**
+     * Unsubscribe from a Site for Points
+     * 
+     * @public
+     * @param {number} siteId - The Site ID
+     */
+    static unsubscribe(siteId)
+    {
+        WebSocketHelper.unsubscribe('site.' + siteId);
+
+        setTimeout(() => {
+            if(isDefined(Points._definitions))
+            {
+                if(siteId in Points._definitions)
+                {
+                    delete Points._definitions[siteId];
+                }
+            }
+
+            if(isDefined(Points._values))
+            {
+                if(siteId in Points._values)
+                {
+                    delete Points._values[siteId];
+                }
+            }
+        }, 3000);
+    }
+
+    /**
+     * Register Events Handler
+     * 
+     * @public
+     * @param {string} event - The Event to Register a Handler for
+     * @param {Function} handler - The Handler Function
+     */
+    static on(event, handler)
+    {
+        if(isDefined(Points._emitter) != true)
+        {
+            Points._emitter = new EventEmitter();
+        }
+
+        Points._emitter.on(event, handler);
+    }
+
+    /**
+     * Register 'readpoints' Event Handler
+     * 
+     * @public
+     * @param {Function} handler - The Handler Function
+     */
+    static onReadPoints(handler)
+    {
+        Points.on('readpoints', handler);
+    }
+
+    /**
+     * Get Point Definition
+     * 
+     * @public
+     * @param {number} siteId - The Site ID
+     * @param {number} pointId - The Point ID
+     * @return {Object} - The Point Definition
+     */
+    static getDefinition(siteId, pointId)
+    {
+        if(siteId in Points._definitions)
+        {
+            if(pointId in Points._definitions[siteId])
+            {
+                return Points._definitions[siteId][pointId];
+            }
+        }
+
+        return undefined;
+    }
+
+    /**
+     * Get Point Value
+     * 
+     * @public
+     * @param {number} siteId - The Site ID
+     * @param {number} pointId - The Point ID
+     * @return {Object} - The Point Value
+     */
+    static getValue(siteId, pointId)
+    {
+        if(siteId in Points._values)
+        {
+            if(pointId in Points._values[siteId])
+            {
+                return Points._values[siteId][pointId];
+            }
+        }
+
+        return undefined;
+    }
+
+    /**
+     * Set Point Value
+     * 
+     * @public
+     * @param {number} siteId - The Site ID
+     * @param {number} pointId - The Point ID
+     * @param {any} value - The Point Value to Write
+     * @return {Promise<String>}
+     */
+    static setValue(siteId, pointId, value)
+    {
+        return new Promise((resolve, reject) => {
+            if(siteId <= 0)
+            {
+                reject(new Error("Invalid Site ID `" + siteId + "`"));
+            }
+
+            if(pointId <= 0)
+            {
+                reject(new Error("Invalid Point ID `" + pointId + "`"));
+            }
+
+            let pointDefinition = Points.getDefinition(siteId, pointId);
+
+            if(isDefined(pointDefinition) != true || pointDefinition.id != pointId)
+            {
+                reject(new Error("Unknown Point ID `" + pointId + "` for Site ID `" + siteId + "`"));
+            }
+
+            WebSocketHelper.emit('createWritePoint', siteId, pointId, value, pointDefinition.valueType, false, (guid) => {
+                resolve(guid);
+            });
+        });
+    }
+
+    /**
+     * Load Point Definitions from the API
+     * 
+     * @private
+     * @param {number} siteId - The Site ID to pull Point Definitions from
+     * @return {Promise<Boolean>}
+     */
+    static loadPointDefinitions(siteId)
+    {
+        if((siteId in Points._definitions) != true)
+        {
+            Points._definitions[siteId] = {};
+        }
+
+        return new Promise((resolve, reject) => {
+            PointController.getAll(siteId)
+            .then((points) => {
+                points.forEach((point) => {
+                    Points._definitions[siteId][point.id] = point;
+                });
+                resolve(true);
+            })
+            .catch((error) => {
+                Points.log(error, 'error');
+                reject(error);
+            });
+        });
+    }
+
+    /**
+     * Load Point Values from the API
+     * 
+     * @private
+     * @param {number} siteId - The Site ID to pull Point Values from
+     * @return {Promise<Boolean>}
+     */
+    static loadPointValues(siteId)
+    {
+        if((siteId in Points._values) != true)
+        {
+            Points._values[siteId] = {};
+        }
+        
+        return new Promise((resolve, reject) => {
+            RequestHelper.getRequest('/sites/' + siteId + '/points/values')
+            .then((pointValues) => {
+                let changedPoints = [];
+                
+                pointValues.forEach((pointValue) => {
+                    if(pointValue.id in Points._values[siteId])
+                    {
+                        Points._values[siteId][pointValue.id] = pointValue;
+
+                        if(Points._values[siteId][pointValue.id].value != pointValue.value || Points._values[siteId][pointValue.id].timestamp != pointValue.timestamp)
+                        {
+                            changedPoints.push(pointValue);
+                        }
+                    }
+                    else
+                    {
+                        Points._values[siteId][pointValue.id] = pointValue;
+                        changedPoints.push(pointValue);
+                    }
+                });
+
+                if(changedPoints.length > 0)
+                {
+                    Points._emitter.emit('readpoints', siteId, changedPoints);
+                }
+
+                resolve(true);
+            })
+            .catch((error) => {
+                Points.log(error, 'error');
+                reject(error);
+            });
+        });
+    }
+
+    /**
+     * Get Default Site ID
+     * 
+     * @private
+     * @return {number}
+     */
+    static getDefaultSiteId()
+    {
+        if(isDefined(Points._definitions) && Object.keys(Points._definitions).length > 0)
+        {
+            return Object.keys(Points._definitions)[0];
+        }
+
+        return undefined;
+    }
+}
+
+export default Points;
---
+++ src/WebSocketHelper.js
@@ -0,0 +1,226 @@
+import io from 'socket.io-client';
+import { isDefined, JWT, WebSocketServer, WebSocketPort, RequestHelper } from './index';
+import { EventEmitter } from 'events';
+
+/**
+ * A Helper Class to abstract away the Lower-Level Socket.IO API to suit calls to the RICADO Gen 4 WebSocket Server
+ *
+ * @public
+ */
+class WebSocketHelper {
+
+    /**
+     * Initialize
+     * 
+     * @package
+     */
+    static initialize()
+    {
+        if(isDefined(WebSocketHelper._emitter) != true)
+        {
+            WebSocketHelper._emitter = new EventEmitter();
+        }
+
+        if(isDefined(WebSocketHelper._subscriptions) != true)
+        {
+            WebSocketHelper._subscriptions = [];
+        }
+        
+        WebSocketHelper._socket = io(WebSocketServer + ':' + WebSocketPort, {
+            transports: ['websocket'],
+            query: {
+                token: JWT
+            }
+        });
+
+        var onEvent = WebSocketHelper._socket.onevent;
+        
+        WebSocketHelper._socket.onevent = function (packet) {
+            var args = packet.data || [];
+            onEvent.call (this, packet);    // original call
+            packet.data = ["*"].concat(args);
+            onEvent.call(this, packet);      // additional call to catch-all
+        };
+
+        WebSocketHelper._socket.on('reconnect_attempt', (attempt) => {
+            // Fired every time we attempt to reconnect.
+            // `attempt` holds the count of attempts so far
+
+            WebSocketHelper._socket.io.opts.transports = ['polling, websocket'];
+
+            WebSocketHelper._socket.io.opts.query = {
+                token: JWT
+            };
+
+            WebSocketHelper.log("Attempting to Re-connect. " + attempt + " attempts so far");
+        });
+
+        WebSocketHelper._socket.on('connect', () => {
+            // Fired when first connected and every subsequent re-connect
+
+            WebSocketHelper.log("Socket Connected");
+
+            if(isDefined(WebSocketHelper._subscriptions))
+            {
+                WebSocketHelper._subscriptions.forEach((subscription) => {
+                    WebSocketHelper._socket.emit('subscribe', subscription);
+                });
+            }
+        });
+
+        WebSocketHelper._socket.on('reconnect', (attempts) => {
+            // Fired when a re-connection attempt is successful
+            // `attempts` holds the count of attempts to get re-connected
+            WebSocketHelper.log("Successfully Re-connected after " + attempts + " attempts");
+        });
+
+        WebSocketHelper._socket.on('disconnect', (reason) => {
+            // Fired whenever the socket is disconnected
+            // The reason will be either server or client
+            log("Socket Disconnected! Reason: " + reason, "warning");
+
+            if(reason == 'io server disconnect')
+            {
+                setTimeout(() => {
+                    WebSocketHelper._socket.connect();
+                }, 5000);
+            }
+        });
+
+        WebSocketHelper._socket.on('ping', () => {
+            // Fired when a Ping Packet is sent to the server.
+            WebSocketHelper.log("~PING~");
+        });
+
+        WebSocketHelper._socket.on('pong', (latency) => {
+            // Fired when a Pong Packet is received from the Server.
+            WebSocketHelper.log("~PONG~ Latency: " + latency + "ms");
+        });
+
+        WebSocketHelper._socket.on('unauthorized', (error) => {
+            // Fired when the JWT Token was invalid or expired
+            WebSocketHelper.log(error);
+        });
+
+        WebSocketHelper._socket.on('error', (error) => {
+            // Fired when an Error Packet is received from the Server.
+            WebSocketHelper.log("Received Error!");
+            WebSocketHelper.log(error);
+        });
+        
+        WebSocketHelper._socket.on('*', (event, ...args) => {
+            WebSocketHelper._emitter.emit(event, ...args);
+        });
+    }
+
+    /**
+     * Loggging
+     * 
+     * @private
+     * @param {string} message - The Message to Log
+     * @param {string} type - The Log Type (defaults to log)
+     */
+    static log(message, type = 'log')
+    {
+        switch(type)
+        {
+            case 'error':
+                console.error('WebSocket :: ' + message);
+                break;
+            
+            case 'warn':
+            case 'warning':
+                console.warn('WebSocket :: ' + message);
+                break;
+            
+            case 'log':
+            default:
+                console.log('WebSocket :: ' + message);
+                break;
+        }
+    }
+
+    /**
+     * Subscribe to a Site or RTU for Events
+     * 
+     * @public
+     * @param {string} key - The Site ID or RTU ID Key (e.g. site.2 or rtu.1200)
+     */
+    static subscribe(key = null)
+    {
+        if(isDefined(WebSocketHelper._subscriptions) != true)
+        {
+            WebSocketHelper._subscriptions = [];
+        }
+
+        if(isDefined(key) != true)
+        {
+            key = 'all';
+        }
+
+        if(WebSocketHelper._subscriptions.includes(key) == false)
+        {
+            WebSocketHelper._subscriptions.push(key);
+        }
+        
+        if(isDefined(WebSocketHelper._socket))
+        {
+            WebSocketHelper._socket.emit('subscribe', key);
+        }
+    }
+
+    /**
+     * Unsubscribe from a Site or RTU for Events
+     * 
+     * @public
+     * @param {string} key - The Site ID or RTU ID Key (e.g. site.2 or rtu.1200)
+     */
+    static unsubscribe(key = null)
+    {
+        if(isDefined(WebSocketHelper._subscriptions) != true)
+        {
+            WebSocketHelper._subscriptions = [];
+        }
+        
+        if(isDefined(key) != true)
+        {
+            key = 'all';
+        }
+
+        if(WebSocketHelper._subscriptions.indexOf(key) >= 0)
+        {
+            WebSocketHelper._subscriptions = WebSocketHelper._subscriptions.splice(WebSocketHelper._subscriptions.indexOf(key), 1);
+        }
+
+        if(isDefined(WebSocketHelper._socket))
+        {
+            WebSocketHelper._socket.emit('unsubscribe', key);
+        }
+    }
+
+    /**
+     * Register Events Handler
+     * 
+     * @public
+     * @param {string} event - The Event to Register a Handler for
+     */
+    static on(event, handler)
+    {
+        if(isDefined(WebSocketHelper._emitter) != true)
+        {
+            WebSocketHelper._emitter = new EventEmitter();
+        }
+
+        WebSocketHelper._emitter.on(event, handler);
+    }
+
+    /**
+     * Emit an Event
+     */
+    static emit(event, ...args)
+    {
+        WebSocketHelper._socket.emit(event, ...args);
+    }
+}
+
+export default WebSocketHelper;
