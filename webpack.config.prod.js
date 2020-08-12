2a1fb90a2678b6a326de2e72c2627f00167d4b23
Josh Biddick <josh@ricado.co.nz>
2020-08-12 17:25:36 +1200

    Bundle TS definitions into a single file
    Add a Webpack Plugin that read the /types directory, resolves imported typings files and bundles them into a single dist/index.d.ts file

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,19 +1,41 @@
-const webpack = require('webpack'); // Access built-in plugins
-const path = require('path');
+const webpack = require("webpack");
+const path = require("path");
+const packageJson = require("./package.json");
 
-module.exports = {
-   entry: './dist/es5/index.js',
-   output: {
-      path: path.resolve(__dirname, 'dist/browser'),
-      filename: 'ricado.api.client.js',
-      library: 'RICADOGen4API',
-      libraryTarget: 'var'
-   },
-   module: {},
-   plugins: [
-      /* new webpack.optimize.UglifyJsPlugin({ mangle: true, sourcemap: false }), // This is now on by default in production mode */
-      new webpack.DefinePlugin({
-         'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-      })
-   ],
+/**
+ * Bundle Typescript declaration files into a single dist/index.d.ts.
+ */
+class DtsBundlePlugin {
+  apply(compiler) {
+    compiler.plugin("done", function () {
+      var dts = require("dts-bundle");
+
+      dts.bundle({
+        name: packageJson.name,
+        main: "types/index.d.ts",
+        out: "../dist/index.d.ts",
+        outputAsModuleFolder: true,
+      });
+    });
+  }
 }
+
+/**
+ * Webpack Config
+ */
+module.exports = {
+  entry: "./dist/es5/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist/browser"),
+    filename: "ricado.api.client.js",
+    library: "RICADOGen4API",
+    libraryTarget: "var",
+  },
+  module: {},
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
+    }),
+    new DtsBundlePlugin(),
+  ],
+};
