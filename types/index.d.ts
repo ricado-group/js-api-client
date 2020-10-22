db47f84b60a55c372a9340ccc8b2968f5a42e9e9
Ash Neilson <ash@ricado.co.nz>
2020-10-22 22:23:31 +1300

    Upgraded Babel and Wrote new Type Definition compilation

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,138 +1,155 @@
 /**
- * Type definitions for Ricado JS Api Client (@ricado/api-client)
- * Project: https://bitbucket.org/ricado/gen4-api-js-client/
- * Definitions by: Josh Biddick <https://github.com/sadsa>
- * TypeScript Version: 3.6.3
+ * Determines if a Variable has been Defined
+ *
+ * @param {any} variable
+ * @api public
+ * @returns {boolean}
  */
-
-import { IPointModel, IPointValueModel } from "./Interfaces";
-
-export * from "./Controllers";
-export * from "./Interfaces";
-export * from "./Models";
-
-export const BaseURL: string;
-
-export const JWT: any;
-
-export const NoAuthPaths: string[];
-
-export const Version: string;
-
-export const WebSocketPort: number;
-
-export const WebSocketServer: string;
-
-export function Points(): void;
-
-export function RequestHelper(): void;
-
-export function WebSocketHelper(): void;
-
-export function apiAccountLogin(key: any, secret: any, ...args: any[]): any;
-
-export function disableDebugMode(): void;
-
+export function isDefined(variable: any): boolean;
+/**
+ * Returns the Initialized Status
+ *
+ * @api public
+ * @returns {boolean}
+ */
+export function isInitialized(): boolean;
+/**
+ * Returns the Debugging Mode
+ *
+ * @api public
+ * @returns {boolean}
+ */
+export function isDebugMode(): boolean;
+/**
+ * Enable Debugging Mode
+ *
+ * @api public
+ */
 export function enableDebugMode(): void;
-
-export function hasToken(): any;
-
+/**
+ * Disable Debugging Mode
+ *
+ * @api public
+ */
+export function disableDebugMode(): void;
 /**
  * Initializes the API Client.
- * If a JSON Web Token is provided, the API Client can be used immediately.
- * If no JWT is provided, a User Account or API Account Login must be performed first.
+ * If a JSON Web Token is provided, no further Authentication steps are required.
+ * If no JWT is provided, a User Account or API Account Login must be completed next.
+ *
+ * @static
+ * @public
+ * @param {string} [token] - An optional JSON Web Token to Initialize the API with.
  */
 export function initialize(token?: string): void;
-
-export function isDebugMode(): any;
-
-export function isDefined(variable: any): any;
-
-export function isInitialized(): any;
-
 /**
- * Locks an existing JWT
+ * Returns whether a valid JSON Web Token exists
+ *
+ * @static
+ * @public
+ * @returns {boolean}
  */
-export function lock(): Promise<boolean>;
-
-export function logout(key?: any, secret?: any): Promise<void>;
-
-/**
- * Unlocks an existing JWT using a Password
- */
-export function passwordUnlock(password: string): Promise<boolean>;
-
-/**
- * Unlocks an existing JWT using a Pin Code
- */
-export function pinCodeUnlock(pinCode: number): Promise<boolean>;
-
-export function ping(): any;
-
+export function hasToken(): boolean;
 /**
  * Generates a new JWT for a User Account
+ *
+ * @static
+ * @public
+ * @param {string} email - The User's Email Address
+ * @param {string} password - The User's Password
+ * @param {string} [providerId] - The Service Provider ID
+ * @return {Promise<string>}
  */
-export function userAccountLogin(
-  email: string,
-  password: string,
-  providerId?: string
-): Promise<string>;
-
-export namespace Points {
-  function getDefaultSiteId(): number;
-
-  function getDefinition(siteId: any, pointId: any): IPointModel | undefined;
-
-  function getValue(siteId: any, pointId: any): IPointValueModel<any> | undefined;
-
-  function initialize(): void;
-
-  function loadPointDefinitions(siteId: any): Promise<boolean>;
-
-  function loadPointValues(siteId: any): Promise<boolean>;
-
-  function log(message: any, ...args: any[]): void;
-
-  function on(
-    event: any,
-    handler: (siteId: string, pointValues: Array<IPointValueModel<any>>) => void
-  ): void;
-
-  function off(event: any, handler: (...args: any[]) => void): void;
-
-  function onReadPoints(handler: any): void;
-
-  function setValue(siteId: any, pointId: any, value: any): Promise<string>;
-
-  function subscribe(siteId: any): Promise<boolean>;
-
-  function unsubscribe(siteId: any): void;
-}
-
-export namespace RequestHelper {
-  function deleteRequest(url: any): any;
-
-  function getRequest(url: any, ...args: any[]): any;
-
-  function patchRequest(url: any, data: any): any;
-
-  function performRequest(method: any, url: any, ...args: any[]): any;
-
-  function postRequest(url: any, data: any): any;
-
-  function putRequest(url: any, data: any): any;
-}
-
-export namespace WebSocketHelper {
-  function emit(event: any, ...args: any[]): void;
-
-  function initialize(...args: any[]): void;
-
-  function log(message: any, ...args: any[]): void;
-
-  function on(event: any, handler: any): void;
-
-  function subscribe(...args: any[]): void;
-
-  function unsubscribe(...args: any[]): void;
-}
+export function userAccountLogin(email: string, password: string, providerId?: string): Promise<string>;
+/**
+ * Generates a new JWT for an API Account
+ *
+ * @static
+ * @public
+ * @param {string} key - The API Key
+ * @param {string} secret - The API Secret
+ * @param {string} [providerId] - The Service Provider ID
+ * @return {Promise<string>}
+ */
+export function apiAccountLogin(key: string, secret: string, providerId?: string): Promise<string>;
+/**
+ * Unlocks an existing JWT using a Pin Code
+ *
+ * @static
+ * @public
+ * @param {string} pinCode - The User's Pin Code
+ * @return {Promise<boolean>}
+ */
+export function pinCodeUnlock(pinCode: string): Promise<boolean>;
+/**
+ * Unlocks an existing JWT using a Password
+ *
+ * @static
+ * @public
+ * @param {string} password - The User's Password
+ * @return {Promise<boolean>}
+ */
+export function passwordUnlock(password: string): Promise<boolean>;
+/**
+ * Locks an existing JWT
+ *
+ * @static
+ * @public
+ * @return {Promise<boolean>}
+ */
+export function lock(): Promise<boolean>;
+/**
+ * Destroys an existing JWT
+ *
+ * @static
+ * @public
+ * @return {Promise<boolean>}
+ */
+export function logout(): Promise<boolean>;
+/**
+ * "Pings" the API
+ *
+ * @static
+ * @public
+ * @return {Promise<boolean>}
+ */
+export function ping(): Promise<boolean>;
+/**
+ * The JSON Web Token for all Authenticated API Calls
+ *
+ * @private
+ * @type {string}
+ */
+export var JWT: string;
+/**
+ * The Base URL for the RICADO Gen 4 API
+ *
+ * @type {string}
+ */
+export var BaseURL: string;
+/**
+ * The WebSocket Server
+ *
+ * @type {string}
+ */
+export var WebSocketServer: string;
+/**
+ * The WebSocket Port
+ *
+ * @type {number}
+ */
+export var WebSocketPort: number;
+/**
+ * The Library Version
+ *
+ * @type {string}
+ */
+export const Version: string;
+import RequestHelper from "./RequestHelper";
+import WebSocketHelper from "./WebSocketHelper";
+import Points from "./Points";
+import NoAuthPaths from "./NoAuthPaths";
+import Controllers from "./Controllers/index";
+import Errors from "./Errors/index";
+import Models from "./Models/index";
+export { RequestHelper, WebSocketHelper, Points, NoAuthPaths, Controllers, Errors, Models };
