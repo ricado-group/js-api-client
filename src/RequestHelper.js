6f9ed17b05fbe91a785991ffc75b25c68b771805
Ash Neilson <ash@ricado.co.nz>
2020-10-30 15:47:06 +1300

    Re-Wrote Request Helper to use the Browser fetch or node-fetch rather than just node-fetch

--- src/RequestHelper.js
+++ src/RequestHelper.js
@@ -1,4 +1,4 @@
-import fetch, { Headers } from 'node-fetch'; // TODO: Ensure this isn't included when packing for web (handle in Webpack, Rollup or Browserify)
+import nodeFetch, { Headers as nodeHeaders, RequestInit as nodeRequestInit, Request as nodeRequest, Response as nodeResponse, Body as nodeBody } from 'node-fetch';
 import { isDefined, isDebugMode, BaseURL, JWT, NoAuthPaths } from './index';
 import BadRequestError from './Errors/BadRequestError';
 import ForbiddenError from './Errors/ForbiddenError';
@@ -26,13 +26,7 @@ class RequestHelper
      */
     static getRequest(url, queryParameters = {})
     {
-        // TODO: Consider returning the promise from performRequest?
-
-        return new Promise((resolve, reject) => {
-            RequestHelper.performRequest('GET', url, null, queryParameters)
-                .then(json => resolve(json))
-                .catch(error => reject(error));
-        });
+        return RequestHelper.performRequest('GET', url, null, queryParameters);
     }
 
     /**
@@ -46,13 +40,7 @@ class RequestHelper
      */
     static putRequest(url, data = null)
     {
-        // TODO: Consider returning the promise from performRequest?
-
-        return new Promise((resolve, reject) => {
-            RequestHelper.performRequest('PUT', url, data)
-                .then(json => resolve(json))
-                .catch(error => reject(error));
-        });
+        return RequestHelper.performRequest('PUT', url, data);
     }
 
     /**
@@ -66,13 +54,7 @@ class RequestHelper
      */
     static patchRequest(url, data = null)
     {
-        // TODO: Consider returning the promise from performRequest?
-
-        return new Promise((resolve, reject) => {
-            RequestHelper.performRequest('PATCH', url, data)
-                .then(json => resolve(json))
-                .catch(error => reject(error));
-        });
+        return RequestHelper.performRequest('PATCH', url, data);
     }
 
     /**
@@ -86,13 +68,7 @@ class RequestHelper
      */
     static postRequest(url, data = null)
     {
-        // TODO: Consider returning the promise from performRequest?
-
-        return new Promise((resolve, reject) => {
-            RequestHelper.performRequest('POST', url, data)
-                .then(json => resolve(json))
-                .catch(error => reject(error));
-        });
+        return RequestHelper.performRequest('POST', url, data);
     }
 
     /**
@@ -106,13 +82,7 @@ class RequestHelper
      */
     static deleteRequest(url, queryParameters = {})
     {
-        // TODO: Consider returning the promise from performRequest?
-
-        return new Promise((resolve, reject) => {
-            RequestHelper.performRequest('DELETE', url, null, queryParameters)
-                .then(result => resolve(result))
-                .catch(error => reject(error));
-        });
+        return RequestHelper.performRequest('DELETE', url, null, queryParameters);
     }
 
     /**
@@ -120,9 +90,9 @@ class RequestHelper
      *
      * @static
      * @public
-     * @param {string} method - The Request Method (e.g. GET, POST, DELETE)
-     * @param {string} url - The Relative URL (e.g. /sites/{siteId}/my-resource)
-     * @param {any} [data] - The Data to send with this Request
+     * @param {string} method The Request Method
+     * @param {string} url The Relative URL
+     * @param {any} [data] The Data to send with this Request
      * @param {Object<string, any>} [queryParameters] - Query Parameters (e.g. {myQuery: myValue})
      * @returns {Promise<any>}
      */
@@ -140,28 +110,9 @@ class RequestHelper
             console.log('RequestHelper :: ' + method.toUpperCase() + " - " + url);
         }
 
-        const options = {
-            method,
-            mode: 'cors',
-            credentials: 'omit',
-            headers: new Headers(),
-        };
-
-        // TODO: Change how NoAuthPaths works to handle * paths (e.g. Use .find() or .filter() and then .contains() on the string)
-        if (isDefined(JWT) && NoAuthPaths.includes(url) === false)
-        {
-            options.headers.set('Authorization', `Bearer ${JWT}`);
-        }
-
-        if (isDefined(data))
-        {
-            options.headers.set('Content-Type', 'application/json');
-            options.body = JSON.stringify(data);
-        }
-
         var query = '';
 
-        if (isDefined(queryParameters) && method == 'GET')
+        if (isDefined(queryParameters) && method.toUpperCase() == 'GET')
         {
             for(const [key, value] of Object.entries(queryParameters))
             {
@@ -179,6 +130,54 @@ class RequestHelper
                 query += (query.length == 0 ? '?' : '&') + key + '=' + parameter;
             }
         }
+
+        if(RequestHelper.hasNativeFetch() === true)
+        {
+            return RequestHelper.performNativeFetchRequest(method, url, data, query);
+        }
+        else
+        {
+            return RequestHelper.performNodeFetchRequest(method, url, data, query);
+        }
+    }
+
+    /**
+     * Perform a Native Fetch Request
+     * 
+     * @private
+     * @static
+     * @param {string} method The Request Method
+     * @param {string} url The Relative URL
+     * @param {any} data The Data to Send with this Request
+     * @param {string} query The Query String
+     * @returns {Promise<any>}
+     */
+    static performNativeFetchRequest(method, url, data, query)
+    {
+        /**
+         * @type {RequestInit}
+         */
+        const options = {
+            method,
+            mode: 'cors',
+            credentials: 'omit',
+        };
+
+        const headers = new Headers();
+
+        // TODO: Change how NoAuthPaths works to handle * paths (e.g. Use .find() or .filter() and then .contains() on the string)
+        if (isDefined(JWT) && NoAuthPaths.includes(url) === false)
+        {
+            headers.set('Authorization', `Bearer ${JWT}`);
+        }
+
+        if (isDefined(data))
+        {
+            headers.set('Content-Type', 'application/json');
+            options.body = JSON.stringify(data);
+        }
+
+        options.headers = headers;
 
         return new Promise((resolve, reject) => {
             fetch(BaseURL + url + query, options)
@@ -235,6 +234,112 @@ class RequestHelper
                 })
                 .catch(error => reject(new NetworkError(error.message)));
         });
+    }
+
+    /**
+     * Perform a Node Fetch Request
+     * 
+     * @private
+     * @static
+     * @param {string} method The Request Method
+     * @param {string} url The Relative URL
+     * @param {any} data The Data to Send with this Request
+     * @param {string} query The Query String
+     * @returns {Promise<any>}
+     */
+    static performNodeFetchRequest(method, url, data, query)
+    {
+        /**
+         * @type {nodeRequestInit}
+         */
+        const options = {
+            method,
+            compress: true,
+        };
+        
+        const headers = new nodeHeaders();
+
+        // TODO: Change how NoAuthPaths works to handle * paths (e.g. Use .find() or .filter() and then .contains() on the string)
+        if (isDefined(JWT) && NoAuthPaths.includes(url) === false)
+        {
+            headers.set('Authorization', `Bearer ${JWT}`);
+        }
+
+        if (isDefined(data))
+        {
+            headers.set('Content-Type', 'application/json');
+            options.body = JSON.stringify(data);
+        }
+
+        options.headers = headers;
+
+        return new Promise((resolve, reject) => {
+            nodeFetch(BaseURL + url + query, options)
+                .then((response) => {
+                    if (response.ok) {
+                        if (response.status === 200 || response.status === 201) {
+                            response.json()
+                                .then((json) => {
+                                    resolve(json);
+                                })
+                                .catch((error) => {
+                                    reject(error);
+                                });
+
+                            return;
+                        }
+
+                        resolve(true);
+                        return;
+                    }
+
+                    response.json().then((json) => {
+                        switch(response.status)
+                        {
+                            case 400:
+                                reject(new BadRequestError(json.title));
+                                break;
+                            
+                            case 401:
+                                reject(new UnauthorizedError(json.title));
+                                break;
+                            
+                            case 403:
+                                reject(new ForbiddenError(json.title));
+                                break;
+                            
+                            case 404:
+                                reject(new NotFoundError(json.title));
+                                break;
+                            
+                            case 405:
+                                reject(new NotAllowedError(json.title));
+                                break;
+                            
+                            case 500:
+                                reject(new ServerError(json.title));
+                                break;
+                            
+                            default:
+                                reject(new Error("Unknown HTTP Response Code `" + response.status + "`"));
+                                break;
+                        }
+                    }).catch(error => reject(error));
+                })
+                .catch(error => reject(new NetworkError(error.message)));
+        });
+    }
+    
+    /**
+     * Checks if a Native Version of Fetch is Available
+     * 
+     * @private
+     * @static
+     * @returns {boolean}
+     */
+    static hasNativeFetch()
+    {
+        return typeof window === 'object' && typeof window.fetch === 'function';
     }
 }
 
