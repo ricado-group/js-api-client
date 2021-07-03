953fad2e8dde9f618dc769471ec9fe8515d759ed
Ash Neilson <ash@ricado.co.nz>
2021-07-04 08:06:23 +1200

    Removed DtsBundle from Webpack Config and run as CLI

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,5 @@
 const webpack = require("webpack");
 const path = require("path");
-const packageJson = require("./package.json");
-
-/**
- * Bundle Typescript declaration files into a single lib/index.d.ts.
- */
-class DtsBundlePlugin {
-    apply(compiler) {
-        compiler.plugin("done", function () {
-            var dts = require("dts-bundle");
-            
-            dts.bundle({
-                name: packageJson.name,
-                main: "types/index.d.ts",
-                out: "../lib/index.d.ts",
-                outputAsModuleFolder: false,
-            });
-        });
-    }
-}
 
 /**
  * Webpack Config
@@ -36,7 +17,6 @@ module.exports = {
         new webpack.DefinePlugin({
             "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
         }),
-        new DtsBundlePlugin(),
     ],
     performance: {
         hints: false
